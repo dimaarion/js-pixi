660e67f98cd13d652772a5c8abf1371d30697b42
dimaarion <dimaarion@gmail.com>
2020-10-18 23:14:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-pixi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pixi)
---
+++ index.js
@@ -0,0 +1,24 @@
+// Import stylesheets
+import "./style.css";
+function getId(e) {
+  return document.getElementById(e);
+}
+function getClass(e,n){
+return document.getElementsByClassName(e)[n];
+}
+
+function createE(e) {
+  return document.body.appendChild(document.createElement(e));
+}
+function childE(e, e2) {
+  return e.appendChild(e2);
+}
+
+function createClass(e, cl) {
+  return (e.className = cl);
+}
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(x, i) {
+  createClass(childE(getId("app"), createE("div")), "imagesBlock");
+  createClass(childE(document.getElementsByClassName('imagesBlock')[i],createE("img")),'images')
+});
