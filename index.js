22ea6d29aecb5f89303d9a85cedd552e21865f97
dimaarion <dimaarion@gmail.com>
2020-10-19 00:57:46 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import stylesheets
 import "./style.css";
+function bagraundImagesPosition(npx){
 function getId(e) {
   return document.getElementById(e);
 }
@@ -17,9 +18,11 @@ function childE(e, e2) {
 function createClass(e, cl) {
   return (e.className = cl);
 }
+
+
 let count = 0;
 [1, 2, 3, 4].map(function(x, i) {
-  count = count + 200;
+  count = count + npx / 2;
   createClass(childE(getId("app"), createE("div")), "imagesBlock");
   createClass(
     childE(document.getElementsByClassName("imagesBlock")[i], createE("img")),
@@ -30,19 +33,27 @@ let count = 0;
 
   
   getClass("images", i).style.position = "absolute";
-  getClass("imagesBlock", i).style.width = "400px";
-  getClass("imagesBlock", i).style.height = "400px";
+  getClass("imagesBlock", i).style.width = npx + "px";
+  getClass("imagesBlock", i).style.height = npx + "px";
   getClass("imagesBlock", i).style.overflow = "hidden";
   getClass("imagesBlock", i).style.position = "relative";
   getClass("imagesBlock", i).style.float = "left";
-  if (i > 0 && i < 3) {
+  if (i > 0 && i < 2) {
     
     getClass("images", i).style.marginLeft = -count + "px";
   }
-  if (i > 2 && i < 3) {
-    
+  if (i > 1 && i < 3) {
     getClass("images", i).style.marginLeft = 0 + "px";
+     getClass("images", i).style.marginTop = -npx + "px";
+  }
+  if (i > 2 && i < 4) {
+    getClass("images", i).style.marginLeft = -count / 2 + "px";
+     getClass("images", i).style.marginTop = -npx + "px";
   }
 });
 
-getId("app").style.width = "800px";
+getId("app").style.width = npx * 2 + "px";
+
+}
+bagraundImagesPosition(400)
+
